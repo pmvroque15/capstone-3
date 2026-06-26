7067889ae6e0e36cead555e99ea758b29aa8169e
pmvroque15 <pmvroque@gmail.com>
2026-06-25 20:00:48 -0400

    Creadet Javadocs comment on top of the method checkout()

--- backend/src/main/java/org/yearup/service/OrderService.java
+++ backend/src/main/java/org/yearup/service/OrderService.java
@@ -29,33 +29,33 @@ public class OrderService {
         this.orderLineItemsRepository = orderLineItemsRepository;
     }
 
-    //TODO make comments for this one
+    /**
+     * Creates a new order for the specified user by converting all items
+     * currently in their shopping cart into order line items.
+     * After order is successfully created, the shopping cart is cleared.
+     *
+     * @param name the username of the authenticated user
+     * @return the newly created order
+     *
+     */
     @Transactional
     public Order checkout(String name) {
 
         User user = userRepository.findByUsername(name);
-
         int userId = user.getId();
 
         Profile userProfile = profileRepository.findByUserId(userId);
 
         Order order = new Order();
-
         order.setUserId(userProfile.getUserId());
-
         order.setDate(LocalDateTime.now());
-
         order.setAddress(userProfile.getAddress());
-
         order.setCity(userProfile.getCity());
-
         order.setState(userProfile.getState());
-
         order.setZip(userProfile.getZip());
-
         order.setShippingAmount(0);
-
         Order savedOrder = orderRepository.save(order);
+
         List<CartItem> items = shoppingCartRepository.findByUserId(userId);
 
         for (CartItem item : items) {
@@ -63,9 +63,7 @@ public class OrderService {
             orderLineItems.setOrderId(order.getOrderId());
             orderLineItems.setProductId(item.getProductId());
             orderLineItems.setQuantity(item.getQuantity());
-
             Product product = productRepository.findById(item.getProductId()).orElseThrow();
-
             orderLineItems.setSalesPrice(product.getPrice());
             orderLineItems.setDiscount(0);
             orderLineItemsRepository.save(orderLineItems);
